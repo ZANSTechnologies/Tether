78fd47a30ba9d54b05d55c19462f20ba058aef81
AMDev80 <aidan.morgan1031@gmail.com>
2026-05-27 01:09:26 -0400

    delete searchColors.php
    renaming file from SearchColors.php to SearchContact.php

--- public/LAMPAPI/SearchContact.php
+++ public/LAMPAPI/SearchContact.php
@@ -1,3 +1,74 @@
-// FIXME complete SearchContact.php.
 <?php
+    // SearchContact.php
+    // Modified from SearchColor.php in-class example.
+	
+    $inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "ContactUser", "ContactPassword123!", "ContactManager");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("select FirstName from Users where FirstName like ? and ID=?");
+		$colorName = "%" . $inData["search"] . "%";
+		$stmt->bind_param("ss", $colorName, $inData["userId"]);
+		$stmt->execute();
+		
+		$result = $stmt->get_result();
+		
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '"' . $row["Name"] . '"';
+		}
+		
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Records Found" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Helper Functions
+	
+    // Fetch JSON. 
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
 ?>
