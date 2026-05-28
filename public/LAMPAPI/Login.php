5716dc881de5579873baad3d2aea633fd259b0e8
AMDev80 <aidan.morgan1031@gmail.com>
2026-05-27 23:18:03 -0400

    Change for our database names

--- public/LAMPAPI/Login.php
+++ public/LAMPAPI/Login.php
@@ -1,5 +1,7 @@
 <?php
 	// Built from our in-class example
+	
+	// Fetch input data from frontend.
 	$inData = getRequestInfo();
 	
 	$id = 0;
@@ -13,11 +15,13 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
+		// Query Database for Credentials.
+		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=? AND Password =?");
 		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
+		// Loop through result set array by row.
 		if( $row = $result->fetch_assoc()  )
 		{
 			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
@@ -31,11 +35,17 @@
 		$conn->close();
 	}
 	
+	////////////////////////////////////////////////////////////////////////////
+    // Helper Functions
+
+	// Fetch input in JSON.
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
+	// Server to Client Response telling browser from the HTTP header that
+	// the message is JSON.
 	function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
