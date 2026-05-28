f1f979c73875a69b0fdffb20d5835469ece336b5
AMDev80 <aidan.morgan1031@gmail.com>
2026-05-28 17:13:07 -0400

    Replace AddColor.php with CreateContact.php

--- public/LAMPAPI/CreateContact.php
+++ public/LAMPAPI/CreateContact.php
@@ -1,3 +1,39 @@
-// FIXME complete CreateContact.php.
 <?php
+	$inputData = getRequestInfo();
+	
+	$color = $inputData["color"];
+	$userId = $inputData["userId"];
+
+	$conn = new mysqli("localhost", "aimo", "T3therT3st", "deletemeAidan");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
+		$stmt->bind_param("ss", $userId, $color);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfSoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
 ?>
