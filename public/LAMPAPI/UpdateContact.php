25e582be4ae41555dde1d4823b9701ff9a663a98
AMDev80 <aidan.morgan1031@gmail.com>
2026-06-02 12:03:15 -0400

    update contact + test on swaggerhub

--- public/LAMPAPI/UpdateContact.php
+++ public/LAMPAPI/UpdateContact.php
@@ -1,3 +1,62 @@
-// FIXME complete UpdateContact.php.
 <?php
+	require_once 'Functions.php'; 
+
+	// Fetch POST request JSON from...
+	$inputData = getRequestInfo();
+    
+    $text = $inputData["text"]
+    $contactID = $inputData["contactID"]
+    $toUpdate = $inputData["toUpdate"];
+    $userColumns = ["FirstName", "LastName", "Phone", "Email"] 
+    if (!in_array($toUpdate, $userColumns) 
+    {
+        ReturnWithError("Invalid Field " . $toUpdate);
+    }
+
+	$connection = new mysqli("localhost", "ContactUser", "ContactPassword123!", "ContactManager"); 	
+	if( $connection->connect_error )
+    {
+        returnWithError( $connection->connect_error );
+    }
+	else
+	{
+		// Prepare the User insert SQL statement.
+		$statement = $connection->prepare("UPDATE Contacts
+                                            SET $toUpdate = ?
+                                            WHERE ID = ?");
+
+		$statement->bind_param("si", $text, $contactID);
+		$statement->execute();
+		# $result = $statement->get_result(); -> An UPDATE does not return a result
+		if ($statement->affected_rows > 0)
+		{
+			returnWithInfo($toUpdate, $text, $contactID);
+		}
+        else if ($statement->affected_rows == 0) 
+        {
+            // In the case it is a non-existant contact
+            returnWithError("Cannot update if contact does not exist.")
+        }
+		else
+		{
+			returnWithError("Could not update "  . $firstName . " " . $lastName . "'s user information.");
+		}
+
+		$statement->close();
+        $connection->close();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Helper Functions
+	function returnWithError( $error )
+	{
+        $returnValue = '{"contactID":0,"updatedField":"","newValue":"","error":"' . $error . '"}';
+		sendResultInfoAsJson( $returnValue );
+	}
+	
+    function returnWithInfo( $field, $newValue, $contactID )
+    {
+        $returnValue = '{"contactID":"",' . $contactID . ',"updatedField":"' . $field . '","newValue":"' . $newValue . '","error":""}';
+        sendResultInfoAsJson( $returnValue );
+    }
 ?>
