22cbca7a8a7606fc95b616a1ce6225d2957983d3
AMDev80 <aidan.morgan1031@gmail.com>
2026-06-06 23:19:38 -0400

    JS script scaffolding

--- public/js/deleteContact.js
+++ public/js/deleteContact.js
@@ -1,4 +1,43 @@
-function deleteContact()
+// Used to delete a contact entry from the database.
+// contactID (int): the ID of the contact to delete.
+// userID (int): the ID of the user that owns the contact. 
+function deleteContact(contactID, userID)
 {
+    // Create JSON string.
+    let temp = {contactID:contactID, userID:userID};
+    let jsonPayload = JSON.stringify(temp);
 
+    let url = urlBase + '/DeleteContact.' + extension;
+
+    // Prepare an asynchronous POST request using the DeleteContact.php script.
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try
+    {
+        xhr.onreadystatechange = function()
+        {
+            if (this.readyState == 4 && this.status == 200)
+            {
+                // JSON from returnWithInfo() or returnWithError() in DeleteContact.php.
+                let jsonObject = JSON.parse(xhr.responseText);
+
+                // "ID":0
+                if (jsonObject.ID < 1)
+                {
+                    document.getElementById("deleteResult").innerHTML = jsonObject.error;
+                    return;
+                }
+
+                // Delete was successful, should update list
+                document.getElementById("deleteResult").innerHTML = "Contact deleted successfully.";
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch(err)
+    {
+        document.getElementById("deleteResult").innerHTML = err.message;
+    }
 }
--- public/js/registerUser.js
+++ public/js/registerUser.js
@@ -1,4 +1,54 @@
+// Used to register a new user into Tether..
+// Called from the register.html.
 function registerUser()
 {
+    // Get registration input.
+    let firstName = document.getElementById("userFirstName").value;
+    let lastName  = document.getElementById("userLastName").value;
+    let login     = document.getElementById("userLogin").value;
+    let password  = document.getElementById("userPassword").value;
 
+    // Create JSON string.
+    let temp = {userFirstName:firstName, userLastName:lastName, userLogin:login, userPassword:password};
+    let jsonPayload = JSON.stringify(temp);
+
+    let url = urlBase + '/Register.' + extension;
+
+    // Prepare an asynchronous POST request using the Register.php script.
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try
+    { 
+        xhr.onreadystatechange = function()
+        {
+            if (this.readyState == 4 && this.status == 200)
+            {
+                // JSON from returnWithInfo() or returnWithError() in Register.php.
+                let jsonObject = JSON.parse(xhr.responseText);
+
+                // "ID":0
+                if (jsonObject.ID < 1)
+                {
+                    document.getElementById("registerResult").innerHTML = jsonObject.error;
+                    return;
+                }
+
+                // Success! Update global like doLogin() behavior.
+                userId    = jsonObject.ID;
+                firstName = jsonObject.FirstName;
+                lastName  = jsonObject.LastName;
+
+                saveCookie();
+
+                window.location.href = "tether.html";
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch(err)
+    {
+        document.getElementById("registerResult").innerHTML = err.message;
+    }
 }
